c7fbcaf2c81cafae50c91735f07fca7f51699155
akbarkidd <akbarkidds@gmail.com>
2025-03-13 23:43:43 +0800

    Update library.lua

--- Fluent/library.lua
+++ Fluent/library.lua
@@ -2770,10 +2770,10 @@ local aa = {
                     if ai.ViewportSize.Y - p.AbsolutePosition.Y < v.AbsoluteSize.Y - 5 then
                         w = v.AbsoluteSize.Y - 5 - (ai.ViewportSize.Y - p.AbsolutePosition.Y) + 40
                     end
-                    v.Position = UDim2.fromOffset(p.AbsolutePosition.X + 20, p.AbsolutePosition.Y - 5 - w)
+                    v.Position = UDim2.fromOffset(p.AbsolutePosition.X + 1, p.AbsolutePosition.Y - 5 - w)
                 end, 0
             local y, z = function()
-                    if #l.Values > 10 then
+                    if #l.Values > 5 then
                         v.Size = UDim2.fromOffset(x, 302)
                     else
                         v.Size = UDim2.fromOffset(x, s.AbsoluteContentSize.Y + 10)
